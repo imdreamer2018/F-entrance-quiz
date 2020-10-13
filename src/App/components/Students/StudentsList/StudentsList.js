75372fc4f3b58c2e73df30c8106f7020fa48d2f0
imdreamer <743295483@qq.com>
2020-10-13 15:23:12 +0800

    [yangqian] fix: student list.

--- src/App/components/Students/StudentsList/StudentsList.js
+++ src/App/components/Students/StudentsList/StudentsList.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import './StudentList.css';
 
 class StudentsList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      students: [],
+    };
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        <h3>
+          学员列表
+        </h3>
+        <div className="student-list">
+          <ul>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+            <li>1.成吉思汗</li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
